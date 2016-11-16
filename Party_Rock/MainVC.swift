943b40819068bfb685edd2687881b4b52c0fd58d
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-16 12:56:33 -0500

    Finish the UI

---
+++ Party_Rock/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Party_Rock
+//
+//  Created by Younoussa Ousmane Abdou on 11/15/16.
+//  Copyright © 2016 Younoussa Ousmane Abdou. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
